c6fba1c04cfb7b2ff76159ee5542f020e6c7ab14
Sam Malikin <workmalikin@gmail.com>
2026-04-05 19:31:15 +0300

    centralize model dependencies
    - Created `deps.py` for models module to unify imports:
        - `Enum` from `enum`
        - `Optional` from `typing`
        - `dataclass` and `asdict` from `dataclasses`
    - Simplifies imports in `enums.py` and `scrape_result.py`

---
+++ src/models/deps.py
@@ -0,0 +1,16 @@
+# ./src/models/deps.py
+
+"""
+Dependencies for the models module.
+"""
+
+from enum import Enum
+from typing import Optional
+from dataclasses import dataclass, asdict
+
+__all__ = [
+    "Enum",
+    "Optional",
+    "dataclass",
+    "asdict",
+]
