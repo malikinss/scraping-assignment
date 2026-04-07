1179eb25c21258e343d46663adabfb8174b5fde0
Sam Malikin <workmalikin@gmail.com>
2026-04-07 17:54:26 +0300

    Update __init__.py

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -4,12 +4,14 @@
 This package contains the services used by the application.
 """
 
-from .input_loader import URLInputLoader
+from .input_loader import URLInputLoader, URL, URLs
 from .content_detector import ContentDetector, detector
 from .metrics import MetricsCalculator, MetricsAggregator, MetricsSummary
 from .result_manager import ResultManager
 
 __all__ = [
+    "URL",
+    "URLs",
     "URLInputLoader",
     "ContentDetector",
     "detector",
